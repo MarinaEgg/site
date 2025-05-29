0b970066481e8dd6493a438c7100f4ea08f0ec7e
Marina <m.jacquet@eggon.fr>
2025-05-29 02:48:57 +0200

    Update sphereFragShader.js
    yellow correction

--- src/components/background/sphereFragShader.js
+++ src/components/background/sphereFragShader.js
@@ -1,5 +1,6 @@
 export default `
 #define PHONG
+
 uniform vec3 diffuse;
 uniform vec3 emissive;
 uniform vec3 specular;
@@ -9,6 +10,7 @@ uniform float time;
 varying vec2 vUv;
 varying vec3 newPosition;
 varying float noise;
+
 #include <common>
 #include <packing>
 #include <color_pars_fragment>
@@ -32,13 +34,18 @@ varying float noise;
 #include <specularmap_pars_fragment>
 #include <logdepthbuf_pars_fragment>
 #include <clipping_planes_pars_fragment>
+
 void main() {
   #include <clipping_planes_fragment>
+
   vec3 color = vec3(vUv * (0.2 - 2.0 * noise), 1.0);
-  vec3 finalColors = vec3(color.r * 1.8, color.r * 1.6, color.b * 0.2);
-  vec4 diffuseColor = vec4(cos(finalColors * noise * 3.0), 1.0);
+  // Pour une teinte jaune, on maximise R et G, on minimise B
+  vec3 finalColors = vec3(1.5 * color.r + 0.5 * noise, 1.5 * color.g + 0.5 * noise, 0.1 * color.b);
+  vec4 diffuseColor = vec4(finalColors, 1.0);
+
   ReflectedLight reflectedLight = ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
   vec3 totalEmissiveRadiance = emissive;
+
   #include <logdepthbuf_fragment>
   #include <map_fragment>
   #include <color_fragment>
@@ -53,12 +60,15 @@ void main() {
   #include <lights_fragment_maps>
   #include <lights_fragment_end>
   #include <aomap_fragment>
-  vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+
+  vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance + finalColors;
+
   #include <envmap_fragment>
   #include <premultiplied_alpha_fragment>
   #include <tonemapping_fragment>
   #include <encodings_fragment>
   #include <fog_fragment>
+
   gl_FragColor = vec4(outgoingLight, diffuseColor.a);
-}
-`;
+}`;
+
